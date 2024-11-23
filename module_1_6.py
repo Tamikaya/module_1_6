f854eeb12ad9b17e5645b0f687bf5405b805236e
Tamikaya <tamikaya111@gmail.com>
2024-11-23 14:39:05 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,19 @@
+#my_dict = {'Xariton':2020,'Platon':2013}
+#print(my_dict)
+#print(my_dict['Xariton'])
+#print(my_dict.get('Anton'))
+#my_dict.update({'Kostya':2009,
+#                'Anisim':2024})
+#print(my_dict)
+#a = my_dict.pop('Kostya')
+#print(my_dict)
+#print(a)
+#print(my_dict)
+my_set = {1,1,1,2,2,3,5,'книга',(3.14159)}
+print(my_set)
+print(my_set.add(4),my_set.add(8))
+print(my_set)
+print(my_set.discard(1))
+print(my_set)      
+
+
